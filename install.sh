0d979a18605c2d38400c85c13e58f198399508ed
nirokay <nirokay-public@protonmail.com>
2026-07-27 10:03:10 +0200

    added hacky install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# This script renames the executables and installs them.
+
+EXEC_PREFIX="k"
+INSTALL_DIR=~/.local/bin
+BIN_DIR=./bin
+
+nimble release
+
+function installFile() {
+    PATH="$*"
+    NAME=$(/usr/bin/basename "$PATH")
+    NEW_NAME="$EXEC_PREFIX$NAME"
+
+    echo "Installing '$NAME' as '$NEW_NAME' to '$INSTALL_DIR'..."
+    /usr/bin/cp "$PATH" "$INSTALL_DIR/$NEW_NAME"
+}
+
+for FILE in ./bin/*; do
+    if [ "$FILE" != ".gitkeep" ]; then
+        installFile "$FILE"
+    fi
+done
